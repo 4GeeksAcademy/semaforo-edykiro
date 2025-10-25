bf1aebac6cd209b88b58d05c5dae15a27ee6bbcf
Eduard Quiroga <edykiro@gmail.com>
2025-10-25 17:04:03 +0200

    Semaforo acabado

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -2,32 +2,47 @@ import React from "react";
 import { use } from "react";
 import { useState } from "react";
 
-const Semaforo = ()=>{
-    
-    const[selectedLight,setSelectedLight] = useState("");
+const Semaforo = () => {
+  const [selectedLight, setSelectedLight] = useState("");
 
+  return (
+    <>
+      <div className="justify-content-center d-flex">
+        <div className="top">
 
-    return(
-        <>  <div className="d-flex justify-content-center">
-                <div className="semaphor p-2 mt-5">
-                    <div 
-                    onClick={()=>setSelectedLight("danger")} 
-                    className={`traffic-light m-3 danger rounded-circle ${selectedLight === "danger" ? "danger-glow":""}`}>
-                    </div>
+        </div>
+        <div className="palo-horizontal">
 
-                    <div
-                    onClick={()=>setSelectedLight("warning")}
-                    className={`traffic-light m-3 warning rounded-circle ${selectedLight == "warning" ? "warning-glow":""}`}>
+        </div>
+        <div className="d-flex palo-semaforo">
+        <p></p>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="semaphor p-2 mt-5">
+            <div
+              onClick={() => setSelectedLight("danger")}
+              className={`traffic-light m-3 danger rounded-circle ${
+                selectedLight === "danger" ? "danger-glow" : ""
+              }`}
+            ></div>
 
-                    </div>
-                    <div 
-                    onClick={()=>setSelectedLight("success")}
-                    className={`traffic-light m-3 success rounded-circle ${selectedLight == "success" ? "success-glow":""}`}>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+            <div
+              onClick={() => setSelectedLight("warning")}
+              className={`traffic-light m-3 warning rounded-circle ${
+                selectedLight == "warning" ? "warning-glow" : ""
+              }`}
+            ></div>
+            <div
+              onClick={() => setSelectedLight("success")}
+              className={`traffic-light m-3 success rounded-circle ${
+                selectedLight == "success" ? "success-glow" : ""
+              }`}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Semaforo
+export default Semaforo;
